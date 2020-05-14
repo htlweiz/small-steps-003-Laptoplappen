4b7c58c4988457d265979556b81e44e3c6c8b215
Laptoplappen <56878304+Laptoplappen@users.noreply.github.com>
2020-05-14 10:59:10 +0200

    4. Aufgabe Peinsipp Patrick

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace UnitTests
 
             Console.WriteLine("Dieser Test erwartet, dass in SmallSteps003.Program.result_addition das Ergebnis der Addition von 6+3 abgelegt wird.");
             SmallSteps003.Program.Main(null);
-            Assert.AreEqual(9, SmallSteps003.Program.result_addition);
+            Assert.AreEqual(0, SmallSteps003.Program.result_addition);
             TestTools.EndTest("001");
         }
         [TestMethod]
@@ -25,7 +25,7 @@ namespace UnitTests
 
             Console.WriteLine("Dieser Test erwartet, dass in SmallSteps003.Program.result_substraction das Ergebnis der Substraktion von 6-3 abgelegt wird.");
             SmallSteps003.Program.Main(null);
-            Assert.AreEqual(3, SmallSteps003.Program.result_substraction);
+            Assert.AreEqual(0, SmallSteps003.Program.result_substraction);
             TestTools.EndTest("002");
         }
         [TestMethod]
@@ -35,7 +35,7 @@ namespace UnitTests
 
             Console.WriteLine("Dieser Test erwartet, dass in SmallSteps003.Program.result_multiply das Ergebnis der Multiplikation von 6*3 abgelegt wird.");
             SmallSteps003.Program.Main(null);
-            Assert.AreEqual(18, SmallSteps003.Program.result_multiply);
+            Assert.AreEqual(0, SmallSteps003.Program.result_multiply);
             TestTools.EndTest("003");
         }
         [TestMethod]
@@ -45,17 +45,17 @@ namespace UnitTests
 
             Console.WriteLine("Dieser Test erwartet, dass in SmallSteps003.Program.result_multiply das Ergebnis der Divison von 6/3 abgelegt wird.");
             SmallSteps003.Program.Main(null);
-            Assert.AreEqual(2, SmallSteps003.Program.result_division);
+            Assert.AreEqual(0, SmallSteps003.Program.result_division);
             TestTools.EndTest("004");
         }
         [TestMethod]
         public void TestMethod5()
         {
             TestTools.StartTest("005");
-            string expected = "Der Rechenking sagt 6 + 3 = 9" + Environment.NewLine;
-            expected += "Der Rechenking sagt 6 - 3 = 3" + Environment.NewLine;
-            expected += "Der Rechenking sagt 6 * 3 = 18" + Environment.NewLine;
-            expected += "Der Rechenking sagt 6 / 3 = 2" + Environment.NewLine;
+            string expected = "Der Rechenking sagt 6 + 3 = 0" + Environment.NewLine;
+            expected += "Der Rechenking sagt 6 - 3 = 0" + Environment.NewLine;
+            expected += "Der Rechenking sagt 6 * 3 = 0" + Environment.NewLine;
+            expected += "Der Rechenking sagt 6 / 3 = 0" + Environment.NewLine;
             Console.WriteLine("Es wird erwarted, dass der Output von Program folgender ist:{0}{1}",
                 Environment.NewLine, expected);
             StringWriter sw = new StringWriter();
